7a591ccaa2a261cd894de633eab37ef13a6dc4fe
Ezza Ardiala <ezzaardiala@gmail.com>
2020-05-01 06:10:05 +0000

    showing error on parsemultiformdata

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -6,7 +6,6 @@ import (
 	"bytes"
 	"net/http"
 	"fmt"
-	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
@@ -26,7 +25,7 @@ func UploadFileToS3(s *session.Session, file multipart.File, fileHeader *multipa
 	// filename, content-type and storage class of the file
 	// you're uploading
 	_, err := s3.New(s).PutObject(&s3.PutObjectInput{
-		Bucket:               aws.String(os.Getenv("BUCKET_NAME")),
+		Bucket:               aws.String("ezza-videos"),
 		Key:                  aws.String(fileHeader.Filename),
 		ACL:                  aws.String("public-read"),// could be private if you want it to be access by only authorized users
 		Body:                 bytes.NewReader(buffer),
@@ -40,7 +39,7 @@ func UploadFileToS3(s *session.Session, file multipart.File, fileHeader *multipa
 		return "", err
 	}
 
-	url := fmt.Sprintf("https://%s.s3-%s.amazonaws.com/%s", os.Getenv("BUCKET_NAME"), os.Getenv("BUCKET_REGION"), fileHeader.Filename)
+	url := fmt.Sprintf("https://%s.s3-%s.amazonaws.com/%s", "ezza-videos", "ap-southeast-1", fileHeader.Filename)
 
  	return url, err
 }
--- main.go
+++ main.go
@@ -10,10 +10,10 @@ import (
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", handler.Home)
-	router.HandleFunc("/resource", handler.PostFile).Methods("POST")
 	headers := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
 	methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"})
 	origins := handlers.AllowedOrigins([]string{"*"})
+	router.HandleFunc("/",handler.Home)
+	router.HandleFunc("/resource", handler.PostFile).Methods("POST")
 	log.Fatal(http.ListenAndServe(":3000", handlers.CORS(headers, methods, origins)(router)))
 }
